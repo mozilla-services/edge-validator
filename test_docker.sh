48c6318b0ef5edd0b91482aa3850c2f0a782b778
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-05-18 13:08:57 -0700

    Add comment to CircleCI documentation

--- test_docker.sh
+++ test_docker.sh
@@ -5,6 +5,8 @@ IMAGE=${IMAGE:-"edge-validator:latest"}
 container_id="$(docker run -tid $IMAGE)"
 docker exec "${container_id}" pipenv run \
     python -m pytest --junitxml=junit.xml tests/
-mkdir -p test-reports
-docker cp "${container_id}":/app/junit.xml test-reports/
+
+# https://circleci.com/docs/2.0/configuration-reference/#store_test_results
+mkdir -p test-reports/pytest/
+docker cp "${container_id}":/app/junit.xml test-reports/pytest/results.xml
 docker stop "${container_id}"

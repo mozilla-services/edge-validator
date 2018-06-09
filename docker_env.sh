c65bdf49dcf784fdebbfae9cf0408db278d5e8b9
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-06-08 17:15:01 -0700

    Copy files instead of mounting a volume

--- docker_env.sh
+++ docker_env.sh
@@ -4,17 +4,9 @@ IMAGE=${IMAGE:-"edge-validator:latest"}
 
 report_path=$(pwd)/"test-reports"
 
-# Create the output report directory if not exits.
-if [ ! -d "${report_path}" ]; then
-    echo "Creating the report path: ${report_path}"
-    mkdir -p "${report_path}"
-    mkdir -p "${report_path}/pytest"
-fi
-
-chown -R 10001:10001 ${report_path}
+container_id="$(docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -tid $IMAGE)"
 cleanup() {
-  # recover ownership of the report folder
-  chown -R $(id -u):$(id -g) ${report_path}
+    docker stop "${container_id}"
 }
 trap cleanup EXIT
 
@@ -25,21 +17,17 @@ REV_B=$3
 
 
 if [ "$CMD" = "test" ]; then
-    docker run \
-        -v ${report_path}:/app/test-reports \
-        -it ${IMAGE} pipenv run \
+    docker exec "${container_id}" pipenv run \
         python -m pytest --junitxml=test-reports/pytest/junit.xml tests/
+    docker cp "${container_id}":/app/test-reports .
 elif [ "$CMD" = "compare" ]; then
     if [ -z "$REV_A" ] || [ -z "$REV_B" ]; then
         echo "Missing arguments REV_A or REV_B!" 1>&2
         exit 1
     fi
-    docker run \
-        -e AWS_ACCESS_KEY_ID \
-        -e AWS_SECRET_ACCESS_KEY \
-        -v ${report_path}:/app/test-reports \
-        -it ${IMAGE} \
-        pipenv run ./integration.py sync compare --report-path test-reports $REV_A $REV_B
+    docker exec "${container_id}" pipenv run \
+        ./integration.py sync compare --report-path test-reports $REV_A $REV_B
+    docker cp "${container_id}":/app/test-reports ${report_path}
 
     diff="${report_path}/${REV_A}-${REV_B}.diff"
     if [ -s ${diff} ]; then
@@ -50,4 +38,3 @@ elif [ "$CMD" = "compare" ]; then
 else
     echo "missing 'test' or 'compare'"
 fi
-
